07a280cac7ab869074e0855ef353612d462855f0
Juanesnc <Juanesnc16@gmail.com>
2025-12-22 09:09:51 -0500

    Cambios

---
+++ backend/database/migrations/2025_12_22_124709_create_filing_old_urgencies_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('filing_old_urgencies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+
+            $table->foreignUuid('rip_invoice_user_id')->constrained();
+            $table->string('numFactura')->nullable();
+            $table->string('Codigo_del_prestador_de_servicios_de_salud')->nullable();
+            $table->string('Tipo_de_identificacion_del_usuario')->nullable();
+            $table->string('numDocumentoIdentificacion')->nullable();
+            $table->string('Fecha_de_ingreso_del_usuario_a_observacion')->nullable();
+            $table->string('Hora_de_ingreso_del_usuario_a_observacion')->nullable();
+            $table->string('Numero_de_autorizacion')->nullable();
+            $table->string('Causa_externa')->nullable();
+            $table->string('Diagnostico_a_la_salida')->nullable();
+            $table->string('Diagnostico_relacionado_Nro_1_a_la_salida')->nullable();
+            $table->string('Diagnostico_relacionado_Nro_2_a_la_salida')->nullable();
+            $table->string('Diagnostico_relacionado_Nro_3_a_la_salida')->nullable();
+            $table->string('Destino_del_usuario_a_la_salida_de_observacion')->nullable();
+            $table->string('Estado_a_la_salida')->nullable();
+            $table->string('Causa_basica_de_muerte_en_urgencias')->nullable();
+            $table->string('Fecha_de_la_salida_del_usuario_en_observacion')->nullable();
+            $table->string('Hora_de_la_salida_del_usuario_en_observacion')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('filing_old_urgencies');
+    }
+};
